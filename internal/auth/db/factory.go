2382a9665435a8477c4678aa757d7bcd696086e4
barantoraman <barantoraman@protonmail.com>
2025-08-07 22:42:19 +0300

    feat: add db factory

--- internal/auth/db/factory.go
+++ internal/auth/db/factory.go
@@ -1,1 +1,26 @@
 package db
+
+import (
+	"errors"
+
+	dbContract "github.com/barantoraman/microgate/internal/auth/db/contract"
+	"github.com/barantoraman/microgate/internal/auth/db/mock"
+	"github.com/barantoraman/microgate/internal/auth/db/postgres"
+	"github.com/barantoraman/microgate/pkg/config"
+)
+
+const (
+	PqType       = "postgres"
+	InMemoryType = "inmemory"
+)
+
+func GetDatabase(cfg config.AuthServiceConfigurations) (dbContract.DBConnection, error) {
+	switch cfg.DBType {
+	case PqType:
+		return postgres.NewPostgresConnection(cfg)
+	case InMemoryType:
+		return mock.NewMockConnection(cfg)
+	default:
+		return nil, errors.New("invalid database type")
+	}
+}
