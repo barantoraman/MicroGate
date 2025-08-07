38f6d999c59930f4761918564a4e02189ea40296
barantoraman <barantoraman@protonmail.com>
2025-08-07 09:55:52 +0300

    chore: format json tags
    Fixes: #2

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -21,34 +21,34 @@ type ApiGatewayServiceConfigurations struct {
 
 type AuthServiceConfigurations struct {
 	DBType       string `mapstructure:"db_type" json:"db_type"`
-	DBUser       string `mapstructure:"db_user"  json:"db_user"`
-	DBPass       string `mapstructure:"db_pass"  json:"db_pass"`
-	DBHost       string `mapstructure:"db_host"  json:"db_host"`
-	DBPort       int    `mapstructure:"db_port"  json:"db_port"`
-	DBName       string `mapstructure:"db_name"  json:"db_name"`
-	DSN          string `mapstructure:"db_dsn"  json:"db_dsn"`
-	MaxOpenConns int    `mapstructure:"db_max_open_conns"  json:"db_max_open_conns"`
-	MaxIdleConns int    `mapstructure:"db_max_idle_conns"  json:"db_max_idle_conns"`
-	MaxIdleTime  string `mapstructure:"db_max_idle_time"  json:"db_max_idle_time"`
-	SSLMode      string `mapstructure:"db_ssl_mode"  json:"db_ssl_mode"`
-	StoreType    string `mapstructure:"store_type"  json:"store_type"`
-	RedisUrl     string `mapstructure:"redis_url"  json:"redis_url"`
-	RedisPass    string `mapstructure:"redis_pass"  json:"redis_pass"`
-	GRPCPort     string `mapstructure:"grpc_port"  json:"grpc_port"`
-	GRPCHost     string `mapstructure:"grpc_host"  json:"grpc_host"`
+	DBUser       string `mapstructure:"db_user" json:"db_user"`
+	DBPass       string `mapstructure:"db_pass" json:"db_pass"`
+	DBHost       string `mapstructure:"db_host" json:"db_host"`
+	DBPort       int    `mapstructure:"db_port" json:"db_port"`
+	DBName       string `mapstructure:"db_name" json:"db_name"`
+	DSN          string `mapstructure:"db_dsn" json:"db_dsn"`
+	MaxOpenConns int    `mapstructure:"db_max_open_conns" json:"db_max_open_conns"`
+	MaxIdleConns int    `mapstructure:"db_max_idle_conns" json:"db_max_idle_conns"`
+	MaxIdleTime  string `mapstructure:"db_max_idle_time" json:"db_max_idle_time"`
+	SSLMode      string `mapstructure:"db_ssl_mode" json:"db_ssl_mode"`
+	StoreType    string `mapstructure:"store_type" json:"store_type"`
+	RedisUrl     string `mapstructure:"redis_url" json:"redis_url"`
+	RedisPass    string `mapstructure:"redis_pass" json:"redis_pass"`
+	GRPCPort     string `mapstructure:"grpc_port" json:"grpc_port"`
+	GRPCHost     string `mapstructure:"grpc_host" json:"grpc_host"`
 }
 
 type TaskServiceConfigurations struct {
 	DBType       string `mapstructure:"db_type" json:"db_type"`
-	DBUser       string `mapstructure:"db_user"  json:"db_user"`
-	DBPass       string `mapstructure:"db_pass"  json:"db_pass"`
-	DBPort       int    `mapstructure:"db_port"  json:"db_port"`
-	DBName       string `mapstructure:"db_name"  json:"db_name"`
-	URI          string `mapstructure:"db_uri"  json:"db_uri"`
-	MaxOpenConns int    `mapstructure:"db_max_open_conns"  json:"db_max_open_conns"`
-	MaxIdleConns int    `mapstructure:"db_max_idle_conns"  json:"db_max_idle_conns"`
-	MaxIdleTime  string `mapstructure:"db_max_idle_time"  json:"db_max_idle_time"`
-	SSLMode      string `mapstructure:"db_ssl_mode"  json:"db_ssl_mode"`
-	GRPCPort     string `mapstructure:"grpc_port"  json:"grpc_port"`
-	GRPCHost     string `mapstructure:"grpc_host"  json:"grpc_host"`
+	DBUser       string `mapstructure:"db_user" json:"db_user"`
+	DBPass       string `mapstructure:"db_pass" json:"db_pass"`
+	DBPort       int    `mapstructure:"db_port" json:"db_port"`
+	DBName       string `mapstructure:"db_name" json:"db_name"`
+	URI          string `mapstructure:"db_uri" json:"db_uri"`
+	MaxOpenConns int    `mapstructure:"db_max_open_conns" json:"db_max_open_conns"`
+	MaxIdleConns int    `mapstructure:"db_max_idle_conns" json:"db_max_idle_conns"`
+	MaxIdleTime  string `mapstructure:"db_max_idle_time" json:"db_max_idle_time"`
+	SSLMode      string `mapstructure:"db_ssl_mode" json:"db_ssl_mode"`
+	GRPCPort     string `mapstructure:"grpc_port" json:"grpc_port"`
+	GRPCHost     string `mapstructure:"grpc_host" json:"grpc_host"`
 }
