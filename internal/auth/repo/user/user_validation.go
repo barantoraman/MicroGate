7278dd9ee47bae730a202e5b7aa5aeef47b7a3d7
barantoraman <barantoraman@protonmail.com>
2025-08-08 10:30:56 +0300

    feat: add user validations

---
+++ internal/auth/repo/user/user_validation.go
@@ -0,0 +1,27 @@
+package user
+
+import (
+	"github.com/barantoraman/microgate/internal/auth/repo/entity"
+	"github.com/barantoraman/microgate/pkg/validator"
+)
+
+func ValidateEmail(v *validator.Validator, email string) {
+	v.Check(email != "", "email", "must be provided")
+	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be valid an email address")
+}
+
+func ValidatePassword(v *validator.Validator, password string) {
+	v.Check(password != "", "password", "must be provided")
+	v.Check(len(password) >= 8, "password", "must be at least 8 bytes long")
+	v.Check(len(password) <= 72, "password", "must not be more than 72 bytes long")
+}
+
+func ValidateUser(v *validator.Validator, u *entity.User) {
+	ValidateEmail(v, u.Email)
+	if u.Password != "" {
+		ValidatePassword(v, u.Password)
+	}
+	if u.PasswordHash == nil {
+		panic("missing password hash for users")
+	}
+}
