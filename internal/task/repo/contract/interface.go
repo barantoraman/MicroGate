3256a7e0f1767210697582391f4743b18bcbd7b9
barantoraman <barantoraman@protonmail.com>
2025-08-09 23:03:52 +0300

    feat: impl mock repo

--- internal/task/repo/contract/interface.go
+++ internal/task/repo/contract/interface.go
@@ -1,4 +1,14 @@
 package contract
 
+import (
+	"context"
+
+	"github.com/barantoraman/microgate/internal/task/repo/entity"
+)
+
 type TaskRepository interface {
+	CreateTask(ctx context.Context, task *entity.Task) error
+	ListTask(ctx context.Context, userID int64) ([]entity.Task, error)
+	DeleteTask(ctx context.Context, taskID string, userID int64) error
+	ServiceStatus(ctx context.Context) error
 }
