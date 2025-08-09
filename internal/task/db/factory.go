7c2fa35fb286847d4e2eb9479f91035a8c3d4842
barantoraman <barantoraman@protonmail.com>
2025-08-09 19:19:00 +0300

    feat: add db layer to task

--- internal/task/db/factory.go
+++ internal/task/db/factory.go
@@ -1,1 +1,26 @@
 package db
+
+import (
+	"errors"
+
+	dbContract "github.com/barantoraman/microgate/internal/task/db/contract"
+	"github.com/barantoraman/microgate/internal/task/db/mock"
+	"github.com/barantoraman/microgate/internal/task/db/mongo"
+	"github.com/barantoraman/microgate/pkg/config"
+)
+
+const (
+	mongoType    = "mongo"
+	InMemoryType = "inmemory"
+)
+
+func GetDatabase(cfg config.TaskServiceConfigurations) (dbContract.DBConnection, error) {
+	switch cfg.DBType {
+	case mongoType:
+		return mongo.NewMongoConnection(cfg)
+	case InMemoryType:
+		return mock.NewMockConnection()
+	default:
+		return nil, errors.New("invalid database type")
+	}
+}
