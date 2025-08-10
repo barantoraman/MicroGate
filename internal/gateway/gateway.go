1baf2e07310cda24c46122d88e4162f1be23e614
barantoraman <barantoraman@protonmail.com>
2025-08-10 14:14:12 +0300

    feat: add SignUp method

--- internal/gateway/gateway.go
+++ internal/gateway/gateway.go
@@ -7,6 +7,7 @@ import (
 
 	authPb "github.com/barantoraman/microgate/internal/auth/pb"
 	authEntity "github.com/barantoraman/microgate/internal/auth/repo/entity"
+	userPkg "github.com/barantoraman/microgate/internal/auth/repo/user"
 	taskPb "github.com/barantoraman/microgate/internal/task/pb"
 	taskEntity "github.com/barantoraman/microgate/internal/task/repo/entity"
 	taskPkg "github.com/barantoraman/microgate/internal/task/repo/task"
@@ -84,6 +85,39 @@ func (a *apiGatewayService) DeleteTask(ctx context.Context, taskID string, userI
 	return nil
 }
 
+// SignUp implements Service.
+func (a *apiGatewayService) SignUp(ctx context.Context, user authEntity.User) (int64, tokenPkg.Token, error) {
+	err := user.Set(user.Password)
+	if err != nil {
+		return 0, tokenPkg.Token{}, errors.New("failed to hash password")
+	}
+
+	v := validator.New()
+	userPkg.ValidateUser(v, &user)
+	if !v.Valid() {
+		return 0, tokenPkg.Token{}, fmt.Errorf("failed to user: %v", v.Errors)
+	}
+
+	usr := authPb.User{
+		Email:    user.Email,
+		Password: user.Password,
+	}
+
+	resp, err := a.authClient.SignUp(ctx, &authPb.SignUpRequest{User: &usr})
+	if err != nil {
+		return 0, tokenPkg.Token{}, errors.New("failed to signup")
+	}
+
+	tkn := tokenPkg.Token{
+		PlainText: resp.Token.PlaintText,
+		Hash:      resp.Token.Hash,
+		UserID:    resp.Token.UserId,
+		Expiry:    resp.Token.Expiry.AsTime(),
+		Scope:     resp.Token.Scope,
+	}
+	return resp.UserId, tkn, nil
+}
+
 // Login implements Service.
 func (a *apiGatewayService) Login(ctx context.Context, user authEntity.User) (int64, tokenPkg.Token, error) {
 	panic("unimplemented")
@@ -91,11 +125,6 @@ func (a *apiGatewayService) Login(ctx context.Context, user authEntity.User) (in
 
 // Logout implements Service.
 func (a *apiGatewayService) Logout(ctx context.Context, token tokenPkg.Token) error {
-	panic("unimplemented")
-}
-
-// SignUp implements Service.
-func (a *apiGatewayService) SignUp(ctx context.Context, user authEntity.User) (int64, tokenPkg.Token, error) {
 	panic("unimplemented")
 }
 
